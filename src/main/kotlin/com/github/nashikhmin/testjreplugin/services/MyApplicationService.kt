b36513c4876f9eb07f04e0f901803384ec07cae7
GitHub Action <action@github.com>
2021-08-23 12:08:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/nashikhmin/testjreplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nashikhmin/testjreplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nashikhmin.testjreplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nashikhmin.testjreplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/nashikhmin/testjreplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nashikhmin/testjreplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nashikhmin.testjreplugin.services
 
+import com.github.nashikhmin.testjreplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
